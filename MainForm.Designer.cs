1217a45c31a8ec4b0479bd86a430597804f604ba
ShrineFox <ShrineFox@users.noreply.github.com>
2023-07-26 12:17:00 -0400

    Improve form layout, save/load JSON, renaming

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,6 +1,9 @@
-﻿namespace P5FlagCompare
+﻿using DarkUI.Forms;
+using DarkUI.Controls;
+
+namespace P5FlagCompare
 {
-    partial class MainForm
+    partial class MainForm : DarkForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -31,67 +34,221 @@
             tlp_Main = new TableLayoutPanel();
             listBox_Comparisons = new ListBox();
             rtb_Output = new RichTextBox();
+            groupBox_NewDisabled = new DarkGroupBox();
+            listBox_NewlyDisabled = new ListBox();
+            groupBox_NewEnabled = new DarkGroupBox();
+            listBox_NewlyEnabled = new ListBox();
+            tlp_Checkboxes = new TableLayoutPanel();
+            chkBox_IDGroups = new DarkCheckBox();
+            lbl_TimeStamp = new DarkLabel();
+            menuStrip_Main = new DarkMenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            loadToolStripMenuItem = new ToolStripMenuItem();
             tlp_Main.SuspendLayout();
+            groupBox_NewDisabled.SuspendLayout();
+            groupBox_NewEnabled.SuspendLayout();
+            tlp_Checkboxes.SuspendLayout();
+            menuStrip_Main.SuspendLayout();
             SuspendLayout();
             // 
             // tlp_Main
             // 
-            tlp_Main.ColumnCount = 2;
-            tlp_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tlp_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tlp_Main.Controls.Add(listBox_Comparisons, 0, 0);
-            tlp_Main.Controls.Add(rtb_Output, 1, 0);
+            tlp_Main.ColumnCount = 3;
+            tlp_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tlp_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
+            tlp_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            tlp_Main.Controls.Add(listBox_Comparisons, 0, 1);
+            tlp_Main.Controls.Add(rtb_Output, 2, 1);
+            tlp_Main.Controls.Add(groupBox_NewDisabled, 1, 2);
+            tlp_Main.Controls.Add(groupBox_NewEnabled, 1, 1);
+            tlp_Main.Controls.Add(tlp_Checkboxes, 1, 3);
             tlp_Main.Dock = DockStyle.Fill;
             tlp_Main.Location = new Point(0, 0);
             tlp_Main.Name = "tlp_Main";
-            tlp_Main.RowCount = 3;
+            tlp_Main.RowCount = 4;
+            tlp_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
             tlp_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
             tlp_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
-            tlp_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
-            tlp_Main.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tlp_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
             tlp_Main.Size = new Size(800, 450);
             tlp_Main.TabIndex = 0;
             // 
             // listBox_Comparisons
             // 
-            listBox_Comparisons.BackColor = SystemColors.MenuText;
+            listBox_Comparisons.BackColor = Color.FromArgb(60, 63, 65);
             listBox_Comparisons.BorderStyle = BorderStyle.None;
             listBox_Comparisons.Dock = DockStyle.Fill;
             listBox_Comparisons.ForeColor = SystemColors.InactiveBorder;
             listBox_Comparisons.FormattingEnabled = true;
             listBox_Comparisons.ItemHeight = 20;
-            listBox_Comparisons.Location = new Point(3, 3);
+            listBox_Comparisons.Location = new Point(3, 48);
             listBox_Comparisons.Name = "listBox_Comparisons";
             tlp_Main.SetRowSpan(listBox_Comparisons, 3);
-            listBox_Comparisons.Size = new Size(394, 444);
+            listBox_Comparisons.Size = new Size(194, 399);
             listBox_Comparisons.TabIndex = 0;
             listBox_Comparisons.SelectedIndexChanged += SelectedComparison_Changed;
+            listBox_Comparisons.KeyDown += Output_Keydown;
             // 
             // rtb_Output
             // 
-            rtb_Output.BackColor = SystemColors.ActiveCaptionText;
+            rtb_Output.BackColor = Color.FromArgb(60, 63, 65);
             rtb_Output.BorderStyle = BorderStyle.None;
             rtb_Output.Dock = DockStyle.Fill;
             rtb_Output.ForeColor = SystemColors.InactiveBorder;
-            rtb_Output.Location = new Point(403, 3);
+            rtb_Output.Location = new Point(483, 48);
             rtb_Output.Name = "rtb_Output";
             rtb_Output.ReadOnly = true;
             tlp_Main.SetRowSpan(rtb_Output, 3);
-            rtb_Output.Size = new Size(394, 444);
+            rtb_Output.Size = new Size(314, 399);
             rtb_Output.TabIndex = 1;
             rtb_Output.Text = "";
             rtb_Output.KeyDown += Output_Keydown;
             // 
-            // Form1
+            // groupBox_NewDisabled
+            // 
+            groupBox_NewDisabled.BorderColor = Color.FromArgb(51, 51, 51);
+            groupBox_NewDisabled.Controls.Add(listBox_NewlyDisabled);
+            groupBox_NewDisabled.Dock = DockStyle.Fill;
+            groupBox_NewDisabled.Location = new Point(203, 228);
+            groupBox_NewDisabled.Name = "groupBox_NewDisabled";
+            groupBox_NewDisabled.Size = new Size(274, 174);
+            groupBox_NewDisabled.TabIndex = 3;
+            groupBox_NewDisabled.TabStop = false;
+            groupBox_NewDisabled.Text = "Newly Disabled Flags";
+            // 
+            // listBox_NewlyDisabled
+            // 
+            listBox_NewlyDisabled.BackColor = Color.FromArgb(70, 63, 65);
+            listBox_NewlyDisabled.BorderStyle = BorderStyle.FixedSingle;
+            listBox_NewlyDisabled.Dock = DockStyle.Fill;
+            listBox_NewlyDisabled.ForeColor = Color.Silver;
+            listBox_NewlyDisabled.FormattingEnabled = true;
+            listBox_NewlyDisabled.ItemHeight = 20;
+            listBox_NewlyDisabled.Location = new Point(3, 23);
+            listBox_NewlyDisabled.Name = "listBox_NewlyDisabled";
+            listBox_NewlyDisabled.Size = new Size(268, 148);
+            listBox_NewlyDisabled.TabIndex = 1;
+            listBox_NewlyDisabled.KeyDown += Output_Keydown;
+            // 
+            // groupBox_NewEnabled
+            // 
+            groupBox_NewEnabled.BorderColor = Color.FromArgb(51, 51, 51);
+            groupBox_NewEnabled.Controls.Add(listBox_NewlyEnabled);
+            groupBox_NewEnabled.Dock = DockStyle.Fill;
+            groupBox_NewEnabled.Location = new Point(203, 48);
+            groupBox_NewEnabled.Name = "groupBox_NewEnabled";
+            groupBox_NewEnabled.Size = new Size(274, 174);
+            groupBox_NewEnabled.TabIndex = 2;
+            groupBox_NewEnabled.TabStop = false;
+            groupBox_NewEnabled.Text = "Newly Enabled Flags";
+            // 
+            // listBox_NewlyEnabled
+            // 
+            listBox_NewlyEnabled.BackColor = Color.FromArgb(60, 73, 65);
+            listBox_NewlyEnabled.BorderStyle = BorderStyle.FixedSingle;
+            listBox_NewlyEnabled.Dock = DockStyle.Fill;
+            listBox_NewlyEnabled.ForeColor = Color.Silver;
+            listBox_NewlyEnabled.FormattingEnabled = true;
+            listBox_NewlyEnabled.ItemHeight = 20;
+            listBox_NewlyEnabled.Location = new Point(3, 23);
+            listBox_NewlyEnabled.Name = "listBox_NewlyEnabled";
+            listBox_NewlyEnabled.Size = new Size(268, 148);
+            listBox_NewlyEnabled.TabIndex = 0;
+            listBox_NewlyEnabled.KeyDown += Output_Keydown;
+            // 
+            // tlp_Checkboxes
+            // 
+            tlp_Checkboxes.ColumnCount = 2;
+            tlp_Checkboxes.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
+            tlp_Checkboxes.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 65F));
+            tlp_Checkboxes.Controls.Add(chkBox_IDGroups, 0, 0);
+            tlp_Checkboxes.Controls.Add(lbl_TimeStamp, 1, 0);
+            tlp_Checkboxes.Dock = DockStyle.Fill;
+            tlp_Checkboxes.Location = new Point(203, 408);
+            tlp_Checkboxes.Name = "tlp_Checkboxes";
+            tlp_Checkboxes.RowCount = 1;
+            tlp_Checkboxes.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tlp_Checkboxes.Size = new Size(274, 39);
+            tlp_Checkboxes.TabIndex = 4;
+            // 
+            // chkBox_IDGroups
+            // 
+            chkBox_IDGroups.AutoSize = true;
+            chkBox_IDGroups.Checked = true;
+            chkBox_IDGroups.CheckState = CheckState.Checked;
+            chkBox_IDGroups.Location = new Point(3, 3);
+            chkBox_IDGroups.Name = "chkBox_IDGroups";
+            chkBox_IDGroups.Size = new Size(89, 24);
+            chkBox_IDGroups.TabIndex = 4;
+            chkBox_IDGroups.Text = "ID Groups";
+            // 
+            // lbl_TimeStamp
+            // 
+            lbl_TimeStamp.AutoSize = true;
+            lbl_TimeStamp.ForeColor = Color.FromArgb(220, 220, 220);
+            lbl_TimeStamp.Location = new Point(98, 0);
+            lbl_TimeStamp.Name = "lbl_TimeStamp";
+            lbl_TimeStamp.Size = new Size(0, 20);
+            lbl_TimeStamp.TabIndex = 5;
+            // 
+            // menuStrip_Main
+            // 
+            menuStrip_Main.BackColor = Color.FromArgb(60, 63, 65);
+            menuStrip_Main.ForeColor = Color.FromArgb(220, 220, 220);
+            menuStrip_Main.ImageScalingSize = new Size(20, 20);
+            menuStrip_Main.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip_Main.Location = new Point(0, 0);
+            menuStrip_Main.Name = "menuStrip_Main";
+            menuStrip_Main.Padding = new Padding(3, 2, 0, 2);
+            menuStrip_Main.Size = new Size(800, 28);
+            menuStrip_Main.TabIndex = 2;
+            menuStrip_Main.KeyDown += Output_Keydown;
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { saveToolStripMenuItem, loadToolStripMenuItem });
+            fileToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(125, 26);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += Save_Click;
+            // 
+            // loadToolStripMenuItem
+            // 
+            loadToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            loadToolStripMenuItem.Size = new Size(125, 26);
+            loadToolStripMenuItem.Text = "Load";
+            loadToolStripMenuItem.Click += Load_Click;
+            // 
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip_Main);
             Controls.Add(tlp_Main);
-            Name = "Form1";
+            Name = "MainForm";
             Text = "P5 Flag Comparer";
+            KeyDown += Output_Keydown;
             tlp_Main.ResumeLayout(false);
+            groupBox_NewDisabled.ResumeLayout(false);
+            groupBox_NewEnabled.ResumeLayout(false);
+            tlp_Checkboxes.ResumeLayout(false);
+            tlp_Checkboxes.PerformLayout();
+            menuStrip_Main.ResumeLayout(false);
+            menuStrip_Main.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -99,5 +256,16 @@
         private TableLayoutPanel tlp_Main;
         private ListBox listBox_Comparisons;
         private RichTextBox rtb_Output;
+        private DarkGroupBox groupBox_NewDisabled;
+        private DarkGroupBox groupBox_NewEnabled;
+        private DarkMenuStrip menuStrip_Main;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private ListBox listBox_NewlyEnabled;
+        private ListBox listBox_NewlyDisabled;
+        private TableLayoutPanel tlp_Checkboxes;
+        private DarkCheckBox chkBox_IDGroups;
+        private DarkLabel lbl_TimeStamp;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,16 +1,28 @@
+using System.Configuration;
 using System.Diagnostics;
 using System.Management;
 using System.Reflection;
 using System.Xml.Linq;
+using DarkUI.Forms;
+using Newtonsoft.Json;
+using ShrineFox.IO;
+using static System.Collections.Specialized.BitVector32;
 
 namespace P5FlagCompare
 {
-    public partial class MainForm : Form
+    public partial class MainForm : DarkForm
     {
-        List<Comparison> comparisons = new List<Comparison>();
+        Settings settings = new Settings();
+        public class Settings
+        {
+            public List<Comparison> comparisons = new List<Comparison>();
+            public List<Tuple<int, string>> mappings = new List<Tuple<int, string>>();
+        }
+
         public class Comparison
         {
             public string Name { get; set; } = "Untitled";
+            public string TimeStamp { get; set; } = "";
             public List<int> EnabledFlags { get; set; } = new List<int>();
             public List<int> NewEnabledFlags { get; set; } = new List<int>();
             public List<int> NewDisabledFlags { get; set; } = new List<int>();
@@ -25,10 +37,69 @@ namespace P5FlagCompare
         {
             if (e.Modifiers == Keys.Control && e.KeyCode == Keys.V)
                 DoComparison();
+
+            if (e.Modifiers == Keys.Control && e.KeyCode == Keys.R)
+            {
+                DoRename(sender);
+            }
+        }
+
+        private void DoRename(object sender)
+        {
+            string ctrlName = ((Control)sender).Name;
+            if (ctrlName == "listBox_NewlyEnabled" || ctrlName == "listBox_NewlyDisabled")
+            {
+                ListBox listBox = (ListBox)sender;
+                int flagId = Convert.ToInt32(listBox.SelectedItem.ToString().Split(' ').Last());
+                RenameFlag(flagId);
+            }
+            else
+                RenameComparison();
+        }
+
+        private void RenameComparison()
+        {
+            RenameForm renameForm = new RenameForm(listBox_Comparisons.SelectedItem.ToString());
+            var result = renameForm.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string newName = renameForm.RenameText;
+                settings.comparisons.First(x => x.Name.Equals(listBox_Comparisons.SelectedItem.ToString())).Name = newName;
+                UpdateForm();
+            }
+        }
+
+        private void RenameFlag(int flagId)
+        {
+            // Get old mapped name if one exists
+            string oldName = "";
+            bool mappingFound = false;
+            if (settings.mappings.Any(x => x.Item1.Equals(flagId)))
+            {
+                mappingFound = true;
+                oldName = settings.mappings.First(x => x.Item1.Equals(flagId)).Item2;
+            }
+
+            RenameForm renameForm = new RenameForm(oldName);
+            var result = renameForm.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string newName = renameForm.RenameText;
+                // Remove existing mapping if one exists
+                if (mappingFound)
+                    settings.mappings.Remove(settings.mappings.First(x => x.Item1.Equals(flagId)));
+                // Add new mapping
+                settings.mappings.Add(new Tuple<int, string>(flagId, newName));
+                // Re-order mappings in numerical order by flag ID
+                settings.mappings = settings.mappings.OrderBy(x => x.Item1).ToList();
+
+                UpdateForm();
+            }
         }
 
         private void DoComparison()
         {
+            // Get last flag dump from clipboard
             if (Clipboard.ContainsText(TextDataFormat.Text))
             {
                 string clipboardText = Clipboard.GetText(TextDataFormat.Text);
@@ -46,7 +117,8 @@ namespace P5FlagCompare
 
         private void AddComparison(string[] clipboardLines, int startLine)
         {
-            Comparison comparison = new Comparison();
+            Comparison comparison = new Comparison() { TimeStamp = DateTime.Now.ToString() };
+
             for (int x = startLine; x < clipboardLines.Length; x++)
             {
                 if (clipboardLines[x].Contains("End Enabled Flag Dump"))
@@ -54,10 +126,10 @@ namespace P5FlagCompare
                 comparison.EnabledFlags.Add(Convert.ToInt32(clipboardLines[x]));
             }
 
-            if (comparisons.Count > 0)
+            if (settings.comparisons.Count > 0)
             {
                 // If the new enabled flags list is missing a flag from the previous dump, display it as removed
-                foreach (var enabledFlag in comparisons.Last().EnabledFlags)
+                foreach (var enabledFlag in settings.comparisons.Last().EnabledFlags)
                 {
                     if (!comparison.EnabledFlags.Any(x => x.Equals(enabledFlag)))
                         comparison.NewDisabledFlags.Add(enabledFlag);
@@ -65,7 +137,7 @@ namespace P5FlagCompare
                 // If a newly enabled flag isn't found in the previous flag dump, display it as added
                 foreach (var enabledFlag in comparison.EnabledFlags)
                 {
-                    if (comparisons.Last().EnabledFlags.Any(x => !x.Equals(enabledFlag)))
+                    if (!settings.comparisons.Last().EnabledFlags.Any(x => x.Equals(enabledFlag)))
                         comparison.NewEnabledFlags.Add(enabledFlag);
                 }
             }
@@ -74,20 +146,24 @@ namespace P5FlagCompare
             // Set unique placeholder name
             int i = 1;
             string name = comparison.Name;
-            while (comparisons.Any(x => x.Name.Equals(comparison.Name)))
+            while (settings.comparisons.Any(x => x.Name.Equals(comparison.Name)))
             {
                 i++;
                 comparison.Name = name + i;
             }
 
-            comparisons.Add(comparison);
-            UpdateForm();
+            // Add to comparison list and update form if different from previous comparison
+            if (comparison.NewEnabledFlags.Count > 0 || comparison.NewDisabledFlags.Count > 0)
+            {
+                settings.comparisons.Add(comparison);
+                UpdateForm();
+            }
         }
 
         private void UpdateForm()
         {
             listBox_Comparisons.Items.Clear();
-            foreach (var comparison in comparisons)
+            foreach (var comparison in settings.comparisons)
             {
                 listBox_Comparisons.Items.Add(comparison.Name);
             }
@@ -97,15 +173,100 @@ namespace P5FlagCompare
 
         private void SelectedComparison_Changed(object sender, EventArgs e)
         {
+            listBox_NewlyEnabled.Items.Clear();
+            listBox_NewlyDisabled.Items.Clear();
             rtb_Output.Clear();
-            if (listBox_Comparisons.SelectedItem != null && comparisons.Any(x => x.Name.Equals(listBox_Comparisons.SelectedItem.ToString())))
+            lbl_TimeStamp.Text = "";
+
+            if (listBox_Comparisons.SelectedItem != null && settings.comparisons.Any(x => x.Name.Equals(listBox_Comparisons.SelectedItem.ToString())))
             {
-                var comparison = comparisons.First(x => x.Name.Equals(listBox_Comparisons.SelectedItem.ToString()));
-                foreach (var enabledFlag in comparison.NewDisabledFlags)
-                    rtb_Output.Text += enabledFlag + "\n";
+                var comparison = settings.comparisons.First(x => x.Name.Equals(listBox_Comparisons.SelectedItem.ToString()));
+
+                foreach (var enabledFlag in comparison.NewEnabledFlags)
+                {
+                    string name = GetFormattedFlag(enabledFlag);
+                    string mappedName = GetMappedName(enabledFlag);
+
+                    listBox_NewlyEnabled.Items.Add(name + mappedName);
+                }
                 foreach (var disabledFlag in comparison.NewDisabledFlags)
-                    rtb_Output.Text += disabledFlag + "\n";
+                {
+                    string name = GetFormattedFlag(disabledFlag);
+                    string mappedName = GetMappedName(disabledFlag);
+
+                    listBox_NewlyDisabled.Items.Add(name + mappedName);
+                }
+
+                // Show list of all enabled flags with names in comments
+                foreach (var enabledFlag in comparison.EnabledFlags)
+                {
+                    string name = GetFormattedFlag(enabledFlag);
+                    string mappedName = GetMappedName(enabledFlag);
+
+                    rtb_Output.Text += $"BIT_ON({name});{mappedName}\n";
+                }
+
+                // Update timestamp
+                lbl_TimeStamp.Text = comparison.TimeStamp;
             }
+        }
+
+        private string GetFormattedFlag(int flagId)
+        {
+            string name = flagId.ToString();
+
+            if (chkBox_IDGroups.Checked)
+            {
+                for (int i = 1; i < Flag.sRoyalBits.Length; i++)
+                {
+                    if (flagId < Flag.sRoyalBits[i])
+                    {
+                        int section = i - 1;
+                        name = $"Flag.Section{section} + {flagId - Flag.sRoyalBits[i - 1]}";
+                        break;
+                    }
+                }
+            }
+
+            return $"{name}";
+        }
+
+        private string GetMappedName(int flagId)
+        {
+            string mappedName = "";
+            if (settings.mappings.Any(x => x.Item1.Equals(flagId)))
+                mappedName = $" // {settings.mappings.First(x => x.Item1.Equals(flagId)).Item2}";
+            return mappedName;
+        }
+
+        private void IdGroups_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateForm();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            var selection = WinFormsEvents.FilePath_Click("Save Project...", true, new string[] { "Project JSON (.json)" }, true);
+            if (selection.Count == 0)
+                return;
+
+            string outPath = selection.First();
+            if (!outPath.ToLower().EndsWith(".json"))
+                outPath += ".json";
+
+            File.WriteAllText(outPath, JsonConvert.SerializeObject(settings, Newtonsoft.Json.Formatting.Indented));
+            MessageBox.Show($"Saved project file to:\n{outPath}", "Preset Project Successfully");
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            var selection = WinFormsEvents.FilePath_Click("Load Project...", true, new string[] { "Project JSON (.json)" });
+            if (selection.Count == 0)
+                return;
+
+            settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(selection.First()));
+
+            UpdateForm();
         }
     }
 }
